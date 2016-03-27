f7f2baac2de04e76c868e75a04c7b94583ce1bdb
harshul1610 <harshulrobo@gmail.com>
2016-03-27 12:30:35 +0530

    issue #12 depth first traversal added

--- pydsa/dfs.py
+++ pydsa/dfs.py
@@ -1,12 +1,23 @@
+# Depth First Search
+# Complexity: O(V+E)
 
 def dfs(graph,s):
     stack = list()
     visited=list()
     stack.append(s)
+    visited.append(s)
     while(len(stack)>0):
         node = stack.pop()
-        visited.append(node)
         for subadjnode in graph[node]:
             if subadjnode not in visited:
+                visited.append(subadjnode)
                 stack.append(subadjnode)
     return visited
+
+graph = {'A':['B', 'C'],
+         'B':['A', 'D', 'E'],
+         'C':['A', 'F'],
+         'D':['B'],
+         'E':['B', 'F'],
+         'F':['C', 'E']}
+print dfs(graph,'A')
