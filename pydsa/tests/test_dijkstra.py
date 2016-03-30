879b3c8406758713d704e1721e3051e01aee433f
Harshul jain <harshulrobo@gmail.com>
2016-03-30 14:09:17 +0530

    update test_dijkstra
    pep8 format done

--- pydsa/tests/test_dijkstra.py
+++ pydsa/tests/test_dijkstra.py
@@ -1,11 +1,12 @@
-from pydsa.dijkstrashortestpath import dijkstra
+from pydsa.dijkstra import dijkstra
+
 
 def test_dijkstra():
-    graph = {'A':[('B','2'),('C','1')],
-             'B':[('A','3'),('D','4'),('E','2')],
-             'C':[('A','1'),('F','4')],
-             'D':[('B','8')],
-             'E':[('B','0'),('F','6')],
-             'F':[('C','4'),('E','7')]}
+    graph = {'A': [('B', '2'), ('C', '1')],
+             'B': [('A', '3'), ('D', '4'), ('E', '2')],
+             'C': [('A', '1'), ('F', '4')],
+             'D': [('B', '8')],
+             'E': [('B', '0'), ('F', '6')],
+             'F': [('C', '4'), ('E', '7')]}
 
-    assert dijkstra(graph,'A','E') == ['A', 'B', 'E']
+    assert dijkstra(graph, 'A', 'E') == ['A', 'B', 'E']
