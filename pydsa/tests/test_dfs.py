c155cf8b3d99fafe0e857ecf1f8ba164865c5a59
Harshul jain <harshulrobo@gmail.com>
2016-03-30 14:08:28 +0530

    update test_dfs
    pep8 format and calling the different way

--- pydsa/tests/test_dfs.py
+++ pydsa/tests/test_dfs.py
@@ -1,10 +1,11 @@
-from pydsa.dfs import dfsearch
+from pydsa.dfs import dfs
+
 
 def test_dfs():
-    graph = {'A':['B', 'C'],
-             'B':['A', 'D', 'E'],
-             'C':['A', 'F'],
-             'D':['B'],
-             'E':['B', 'F'],
-             'F':['C', 'E']}
-    assert dfsearch(graph, 'A') == ['A', 'B', 'C', 'F', 'E', 'D']
+    graph = {'A': ['B', 'C'],
+             'B': ['A', 'D', 'E'],
+             'C': ['A', 'F'],
+             'D': ['B'],
+             'E': ['B', 'F'],
+             'F': ['C', 'E']}
+    assert dfs(graph, 'A') == ['A', 'B', 'C', 'F', 'E', 'D']
